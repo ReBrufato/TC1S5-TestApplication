bcba9367381b83e90da92cbbb4dd990d31104074
ReBrufato <brufato17.renan@gmail.com>
2023-11-19 13:14:18 -0300

    test: methods setup and quitWebDriver implemented

--- testapppawnsandcare/src/test/java/Tests.java
+++ testapppawnsandcare/src/test/java/Tests.java
@@ -1,3 +1,24 @@
-public class Tests {
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class Tests {
+    private WebDriver driver = null;
+    private Faker faker = new Faker();
+
+    @BeforeEach
+    void setup() throws InterruptedException {
+        if(driver == null){
+            //WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+    }
+
+    @AfterEach
+    public void quitWebDriver(){
+        if(driver != null) driver.quit();
+    }
 }
