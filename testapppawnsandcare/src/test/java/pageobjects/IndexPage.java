b071384129e7c2dfb469a7d454fe0316fa3e6fcd
ReBrufato <brufato17.renan@gmail.com>
2023-11-19 14:08:38 -0300

    feat: created method getPagePet in class IndexPage

--- testapppawnsandcare/src/test/java/pageobjects/IndexPage.java
+++ testapppawnsandcare/src/test/java/pageobjects/IndexPage.java
@@ -1,4 +1,33 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class IndexPage {
+    private WebDriver driver;
+
+    private final String urlApp = "http://localhost:3000/";
+
+    public IndexPage(WebDriver driver){
+        this.driver = driver;
+        driver.get(urlApp);
+    }
+
+    public String getPagePet() throws InterruptedException {
+        final WebElement linkPet = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div[1]/div[2]/div/div/a[2]")));
+
+        linkPet.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.titleIs("Paws & Care | Pets"));
+
+        final String titlePage = driver.getTitle();
+        return titlePage;
+    }
 }
