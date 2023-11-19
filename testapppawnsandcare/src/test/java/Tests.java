483cf5d0e1d8651164ed0f0e38ea913e3cfb3e39
ReBrufato <brufato17.renan@gmail.com>
2023-11-19 13:25:16 -0300

    test: shouldPerformRequisitionToApp test created

--- testapppawnsandcare/src/test/java/Tests.java
+++ testapppawnsandcare/src/test/java/Tests.java
@@ -1,9 +1,12 @@
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class Tests {
     private WebDriver driver = null;
@@ -21,4 +24,12 @@ public class Tests {
     public void quitWebDriver(){
         if(driver != null) driver.quit();
     }
+
+    @Test
+    @DisplayName("shouldPerformRequisitionToApp")
+    void testGetToApp() throws InterruptedException {
+        driver.get("http://localhost:3000/");
+        Thread.sleep(1500);
+    }
+
 }
